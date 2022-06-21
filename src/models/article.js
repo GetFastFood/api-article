30d409965022150bdad1b563469b19e95ef9698e
VictorM <victor.massotte@viacesi.fr>
2022-06-21 14:41:32 +0200

    Update article model (remove category)

--- src/models/article.js
+++ src/models/article.js
@@ -3,10 +3,9 @@ const mongoose = require('../database/index'), Schema = mongoose.Schema;
 const article_schema = new Schema({
     id: Schema.Types.ObjectId, 
     name: String,
-    image: Array,   
+    image: String,   
     description: String,
     price: Number,
-    category: String,
     type: String,
     versionKey: false,
 });
